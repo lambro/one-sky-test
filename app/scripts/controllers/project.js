5293a7e85536dfa880665919853a84e7c86809e7
Laura Ambrose <lauraambrose@gmail.com>
2017-03-20 16:38:54 +0800

    added hover state to inbox project list

--- app/scripts/controllers/project.js
+++ app/scripts/controllers/project.js
@@ -8,7 +8,15 @@
  * Controller of the oneSkyTestApp
  */
 angular.module('oneSkyTestApp')
-  .controller('ProjectCtrl', ['$scope', function ($scope) {
+  .controller('ProjectCtrl', ['$scope', 'moment', function ($scope, moment) {
+    // creating some fake times
+    var vm = this;
+    vm.timeNow = moment();
+    vm.timeNow2 = moment().subtract(1, 'hours');
+    vm.timeNow3 = moment().subtract(1, 'days');
+    vm.timeNow4 = moment().subtract(2, 'days');
+    vm.timeNow5 = moment().subtract(4, 'days');
+
     $scope.example = 'sausages';
     $scope.projects = [
       {'id': 1, 'name':'Project One', 'read': true},
@@ -19,25 +27,50 @@ angular.module('oneSkyTestApp')
         'language':'Chinese', 
         'image': 'https://goo.gl/KhNkFs',
         'content': 'Should you have any problem with the translation, feel free to contact the project manager!',
-        'date': '12-01-2016','sender': 'A Chan',
+        'date': (vm.timeNow2.format('H:mma')) + ' (' + (vm.timeNow2.fromNow()) + ')',
+        'sender': 'A Chan',
         'read': true
       },
       {'id': 2, 'language':'French',
         'image': 'https://goo.gl/KhNkFs',
         'content': 'Should you have any problem with the translation, feel free to contact the project manager!',
-        'date': '12-01-2016', 'sender': 'A Chan',
+        'date': (vm.timeNow3.format('MMM YY')) + ' (' + (vm.timeNow3.fromNow()) + ')',
+        'sender': 'A Chan',
         'read': false
       },
       {'id': 3, 'language':'Korean',
         'image': 'https://goo.gl/KhNkFs',
         'content': 'Should you have any problem with the translation, feel free to contact the project manager!',
-        'date': '12-01-2016', 'sender': 'A Chan',
+        'date': (vm.timeNow4.format('MMM YY')) + ' (' + (vm.timeNow4.fromNow()) + ')',
+        'sender': 'A Chan',
         'read': true}];
     $scope.chatRecords = [
-      {'content':'Should you have any problem with the translation, feel free to contact the project manager!', 'image': 'https://goo.gl/xwPb0E', 'sender': 'A Chan', 'me': true},
-      {'content':'Should you have any problem with the translation, feel free to contact the project manager!', 'image': 'https://goo.gl/KhNkFs', 'sender': 'A Chan', 'me': false},
-      {'content':'Should you have any problem with the translation, feel free to contact the project manager!', 'image': 'https://goo.gl/xwPb0E', 'sender': 'A Chan', 'me': true},
-      {'content':'Should you have any problem with the translation, feel free to contact the project manager!', 'image': 'https://goo.gl/KhNkFs', 'sender': 'A Chan', 'me': false}];
+      {
+        'content':'Should you have any problem with the translation, feel free to contact the project manager!', 
+        'image': 'https://goo.gl/xwPb0E',
+        'sender': 'A Chan', 'me': true,
+        'date': (vm.timeNow4.format('MMM YY')) + ' (' + (vm.timeNow4.fromNow()) + ')',
+      },
+      {
+        'content':'Should you have any problem with the translation, feel free to contact the project manager!',
+        'image': 'https://goo.gl/KhNkFs',
+        'sender': 'A Chan',
+        'me': false,
+        'date': (vm.timeNow3.format('MMM YY')) + ' (' + (vm.timeNow3.fromNow()) + ')',
+      },
+      {
+        'content':'Should you have any problem with the translation, feel free to contact the project manager!',
+        'image': 'https://goo.gl/xwPb0E',
+        'sender': 'A Chan',
+        'me': true,
+        'date': (vm.timeNow2.format('H:mma')) + ' (' + (vm.timeNow2.fromNow()) + ')',
+      },
+      {'content':'Should you have any problem with the translation, feel free to contact the project manager!',
+        'image': 'https://goo.gl/KhNkFs',
+        'sender': 'A Chan',
+        'me': false,
+        'date': (vm.timeNow2.format('H:mma')) + ' (' + (vm.timeNow2.fromNow()) + ')',
+      }];
   
       $scope.setProject = function(name) {
         $scope.currentProject = name;
